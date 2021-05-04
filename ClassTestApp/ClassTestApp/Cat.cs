065fd4c0fc6f10d983ac0ea042c48ec9dae336ff
EolKimsss <gogosinggg@nate.com>
2021-05-04 12:47:39 +0900

    20210504오전

---
+++ ClassTestApp/ClassTestApp/Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestApp
+{
+    class Cat : Animal
+    {
+
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public Cat() { }
+        public Cat(string name, string color, int age)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Age = age;
+        }
+        public Cat(string name, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+        }
+        public Cat(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public void Meow() { Console.WriteLine($"{this.Color} {this.Name} {this.Age} 야옹!");}
+
+        public override void Sleep()
+        {
+            base.Sleep(); // 부모 Sleep() 실행
+            Console.WriteLine($"{this.Color}고양이 {this.Name}이(가) 쿨쿨잡니다.");
+        }
+    }
+}
---
+++ ClassTestApp/ClassTestApp/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+// 
+
+namespace ClassTestApp
+{
+    class MainApp
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("고양이 객체 생성 :");
+            Cat kitty = new Cat("키티",3);
+            kitty.Age = 3;
+            kitty.Color = "하얀색";
+            kitty.Meow();
+
+            Cat nero = new Cat("네로",12)
+            {
+                Color = "검정"
+            };
+            nero.Meow();
+
+            Cat mimi = new Cat("mimi", "노랑", 3);
+            mimi.Meow();
+
+            Cat aaa = new Cat("aa", "흰색");
+            aaa.Meow();
+            aaa.Sleep();
+
+            var kkk = (123,456.345,"aaa",aaa);
+            Console.WriteLine(kkk.Item1.GetType()) ;
+            Console.WriteLine(kkk.Item2.GetType());
+            Console.WriteLine(kkk.Item3.GetType());
+           // Console.WriteLine(kkk.Item4.GetType());
+            kkk.Item1 ++;
+            
+
+        }
+    }
+}
